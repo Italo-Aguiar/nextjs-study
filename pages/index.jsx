1932df398a66b1317a42193e6379f0a22cef4eb0
Italo Aguiar <italoaa99@gmail.com>
2020-12-01 18:52:12 -0300

    removing test counter

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,25 +1,7 @@
-import { useState } from 'react';
-
 const Home = () => {
   return (
     <div>
       <h2>Home</h2>
-      <Contador /> 
-    </div>
-  );
-}
-
-const Contador = () => {
-  const [ counter, setCounter ] = useState(1);
-
-  const incrCounter = () => {
-    setCounter(counter + 1);
-  }
-
-  return (
-    <div>
-      <p>{counter}</p>
-      <button onClick={incrCounter}>Adicionar</button>
     </div>
   );
 }
